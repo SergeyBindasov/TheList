9cdace973c86dbc81f804ccb5245959e33e7de96
Sergey <bindasov.sergey@gmail.com>
2022-01-20 12:06:06 +0300

    SearchBar
    search, queue, dismiss

--- TheList/Controllers/TheListViewController.swift
+++ TheList/Controllers/TheListViewController.swift
@@ -15,7 +15,12 @@ class TheListViewController: UIViewController {
     
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
-    //let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
+    private lazy var searchBar: UISearchBar = {
+        let bar = UISearchBar()
+        bar.delegate = self
+        return bar
+    }()
     
     private lazy var myTableView: UITableView = {
         let table = UITableView()
@@ -30,7 +35,7 @@ class TheListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.addSubviews(myTableView)
+        view.addSubviews(searchBar, myTableView)
         view.backgroundColor = UIColor(red: 0.47, green: 0.44, blue: 0.65, alpha: 1.00)
         setupLayout()
         loadItems()
@@ -50,34 +55,7 @@ class TheListViewController: UIViewController {
     }
 }
 
-extension TheListViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-                
-        array[indexPath.row].isDone = !array[indexPath.row].isDone
-        
-        saveItem()
-        tableView.deselectRow(at: indexPath, animated: true)
-    }
-}
-
-extension TheListViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return array.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        let item = array[indexPath.row]
-        cell.backgroundColor = .white
-        cell.textLabel?.text = item.title
-        // ternary operator
-        cell.accessoryType = item.isDone ? .checkmark : .none
-        return cell
-    }
-}
-
-
-
+// MARK: -  TheListVC methods
 extension TheListViewController {
     
     func saveItem() {
@@ -90,14 +68,15 @@ extension TheListViewController {
         myTableView.reloadData()
     }
     
-    func loadItems() {
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
 
-        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        
         do {
             array = try context.fetch(request)
         } catch {
             print("error fetching data from context \(error)")
         }
+        myTableView.reloadData()
     }
     
     func destroyItem(indexPath: Int) {
@@ -132,8 +111,67 @@ extension TheListViewController {
     
     func setupLayout() {
         
+        searchBar.snp.makeConstraints { make in
+            make.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+        }
+        
         myTableView.snp.makeConstraints { make in
-            make.edges.equalTo(view.safeAreaLayoutGuide)
+            make.top.equalTo(searchBar.snp.bottom)
+            make.leading.trailing.bottom.equalTo(view.safeAreaLayoutGuide)
         }
     }
 }
+
+// MARK: - Search bar methods
+extension TheListViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+       
+        loadItems(with: request)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+           
+        }
+    }
+}
+
+// MARK: - TableView methods
+extension TheListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+                
+        array[indexPath.row].isDone = !array[indexPath.row].isDone
+        
+        saveItem()
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+extension TheListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let item = array[indexPath.row]
+        cell.backgroundColor = .white
+        cell.textLabel?.text = item.title
+        // ternary operator
+        cell.accessoryType = item.isDone ? .checkmark : .none
+        return cell
+    }
+}
+
+
+
+
