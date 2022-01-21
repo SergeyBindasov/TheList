ea40c98dcfcc45cfc95ca1e196247839aba90f95
Sergey <bindasov.sergey@gmail.com>
2022-01-21 15:58:22 +0300

    Realm
    Pods install

--- TheList/AppDelegate.swift
+++ TheList/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import CoreData
+import RealmSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,7 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
+        
+        print(Realm.Configuration.defaultConfiguration.fileURL)
+        
+        do {
+        let realm = try Realm()
+        } catch {
+            print("error installing new realm \(error)")
+        }
         return true
     }
 
